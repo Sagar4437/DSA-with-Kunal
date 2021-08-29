3033cc5e57bd877f3b8c7fa0e52244d361864b10
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-08-29 12:54:16 +0530

    Delete 1920. Build Array from Permutation | Leetcode.py

--- 1920. Build Array from Permutation | Leetcode.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        arr = []
-        for i in range(len(nums)):
-            val = nums[nums[i]]
-            arr.append(val)
-            
-        return arr
-        
