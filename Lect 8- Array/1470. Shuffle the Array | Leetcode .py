04e24291893f081126c5bd636de26e6bab586dea
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-08-29 20:32:20 +0530

    Create 1470. Shuffle the Array | Leetcode .py

---
+++ Lect 8- Array/1470. Shuffle the Array | Leetcode .py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr =[]
+        i = 0
+        while i<n:
+            arr.append(nums[i])            
+            arr.append(nums[n+i])
+            i += 1
+        return arr
