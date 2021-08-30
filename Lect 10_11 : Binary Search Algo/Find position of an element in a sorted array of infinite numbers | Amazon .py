ed50899c6482d81c843bde5fb159bcf0d84b1ad7
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-08-30 23:22:29 +0530

    Update Find position of an element in a sorted array of infinite numbers | Amazon .py

--- Lect 10_11 : Binary Search Algo/Find position of an element in a sorted array of infinite numbers | Amazon .py
+++ Lect 10_11 : Binary Search Algo/Find position of an element in a sorted array of infinite numbers | Amazon .py
@@ -1,3 +1,5 @@
+# Q: https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
 #same binary search code
 def search(nums,target,si,ei):
 	while si <= ei:
@@ -13,18 +15,20 @@ def search(nums,target,si,ei):
 
 	return -1
 
+def index(arr,target):
+	# we dont know the end index hence apply binary search on chunk/bunch
+	si = 0
+	ei = 1
+	while target > arr[ei]:
+		new_si = ei + 1
+
+		# new_end = prev_end + box_size * 2
+		ei = ei + (ei - si + 1)*2
+		si = new_si
+
+	return search(arr,target,si,ei)
+
 if __name__ == '__main__':
 	arr = [1,2,4,6,7,8,9,10,14,16,17,22,24,26,29,30]
-	
-	# we dont know the end index hence apply binary search on chunk/bunch
-	si = 1
-	ei = 2 
-
-	while True:
-		# print(si-1,ei-1)
-		val = search(arr,30,si-1,ei-1)
-		if val != -1:
-			print('index = ',val)
-			break
-		si = si * 2
-		ei = ei * 2
+	target = 22
+	print(index(arr,target))
