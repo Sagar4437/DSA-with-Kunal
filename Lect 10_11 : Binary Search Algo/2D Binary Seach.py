62b140ac24c6ccf07a93ed6b1c25b124b28f688a
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-09-12 12:45:50 +0530

    Create 2D Binary Seach.py

---
+++ Lect 10_11 : Binary Search Algo/2D Binary Seach.py
@@ -0,0 +1,18 @@
+def binary_2d_search(matrix,target):
+	r = 0
+	c = len(matrix) - 1
+
+	while r < len(matrix) and c >= 0:
+		if matrix[r][c] == target:
+			return [r,c]
+
+		elif matrix[r][c] < target:
+			r += 1
+
+		else:
+			c -= 1
+
+	return [-1,-1]
+
+matrix = [[10,20,30],[40,50,60],[70,80,90]]
+print(binary_2d_search(matrix,80))
