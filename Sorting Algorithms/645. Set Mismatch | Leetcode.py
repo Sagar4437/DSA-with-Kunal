ed4299c86dfa7e9703b1c80c04cb2f4712bdebad
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-09-13 14:54:04 +0530

    Create 645. Set Mismatch | Leetcode.py

---
+++ Sorting Algorithms/645. Set Mismatch | Leetcode.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, arr: List[int]) -> List[int]:
+        i = 0
+        while i < len(arr):
+
+            ci = arr[i]-1
+            if arr[i] > len(arr):
+                i += 1
+            elif arr[ci] == arr[i]:
+                i += 1
+            else:
+                arr[i],arr[ci] = arr[ci], arr[i]
+                
+        r = []
+        for i in range(len(arr)):
+            if i+1 != arr[i]:
+                return [arr[i],i+1]
