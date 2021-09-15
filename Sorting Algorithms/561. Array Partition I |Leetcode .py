f45c3170661ab1819075fbb87fd85acc92076ae5
Sagar Darade <89535768+Sagar4437@users.noreply.github.com>
2021-09-15 15:43:55 +0530

    Create 561. Array Partition I |Leetcode .py

---
+++ Sorting Algorithms/561. Array Partition I |Leetcode .py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        sum1 = 0
+        for ele in range(0,len(nums),+2):
+            sum1 += nums[ele]
+        return sum1
